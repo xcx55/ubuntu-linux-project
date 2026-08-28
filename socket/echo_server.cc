fdf676c1825b47581df1b90d87d3a80efe30f568
熊帝天 <18383347618@163.com>
2026-08-28 12:29:23 +0800

    套接字通信

--- socket/echo_server.cc
+++ socket/echo_server.cc
@@ -2,11 +2,12 @@
 #include"echo_server.hpp"
 #include<memory>
 int main(int argc,char* argv[]){
-    if(argc!=3){
+    if(argc!=2){
         exit(usag_erron);
     }
-    std::string ip=argv[1];
-    uint16_t port=std::stoi(argv[2]);
-    std::unique_ptr<Udpserver> usr=std::make_unique<Udpserver>(ip,port);
+    uint16_t port=std::stoi(argv[1]);
+    std::unique_ptr<Udpserver> usr=std::make_unique<Udpserver>(port);
+    usr->init();
+    usr->start();
     return 0;
 }
--- socket/echo_server.hpp
+++ socket/echo_server.hpp
@@ -18,9 +18,9 @@ enum exit
 class Udpserver
 {
 public:
-    Udpserver(std::string &p, uint16_t port)
+    Udpserver(uint16_t port)
         : _sockfd(-1),
-          _ip(p),
+        //   _ip(p),
           _port(port)
     {
     }
@@ -33,19 +33,58 @@ public:
             std::cerr << "socket error" << std::endl;
             exit(sock_erron);
         }
+        else
+        {
+            std::cout << "create socket file success" << std::endl;
+        }
         struct sockaddr_in local;
-        bzero(&local,sizeof(sockaddr_in));
-        local.sin_family=AF_INET;
-        local.sin_port=htons(_port);
-        local.sin_addr.s_addr=inet_addr(_ip.c_str());
-
-        int n=bind(_sockfd,(sockaddr*)&local,sizeof(local));
+        bzero(&local, sizeof(sockaddr_in));
+        local.sin_family = AF_INET;
+        local.sin_port = htons(_port);
+        local.sin_addr.s_addr = INADDR_ANY;
+        // local.sin_addr.s_addr=inet_addr(_ip);
+        int n = bind(_sockfd, (sockaddr *)&local, sizeof(local));
+        if (n == 0)
+        {
+            std::cout << "bind success" << std::endl;
+        }
+        else
+        {
+            std::cout << "bind errno" << std::endl;
+            exit(0);
+        }
+    }
+    void start()
+    {
+        while (true)
+        {
+            char inbuff[1024];
+            sockaddr_in in;
+            socklen_t len = sizeof(in);
+            // 需要用户套接字信息！
+            int n = recvfrom(_sockfd, inbuff, sizeof(inbuff) - 1, 0, (sockaddr *)&in, &len);
+            inbuff[n] = 0;
+            int port = ntohs(in.sin_port);
+            std::string pq = inet_ntoa(in.sin_addr);
+            std::string p1 = "[" + pq + ":" + std::to_string(port) + "#" + "]";
+            if (n > 0)
+            {
+                std::cout << "client say#" << p1 << inbuff << std::endl;
+                std::string server_echo = "server say#";
+                server_echo += inbuff;
+                sendto(_sockfd, server_echo.c_str(), server_echo.size(), 0, (sockaddr *)&in, len);
+            }
+            else
+            {
+                std::cerr << "recvfrom erron" << std::endl;
+            }
+        }
     }
     void loop() {}
 
 private:
     int _sockfd;
-    std::string _ip;
+    // std::string _ip;
     uint16_t _port;
 };
 
